620a2284d9f6ea5dd1ed650dfa36ed6a72a7db92
Aline Silveira <aline@outlook.ie>
2024-03-19 20:44:27 -0300

    atualizado exercicio 15

--- exercicios/exercicio_15.py
+++ exercicios/exercicio_15.py
@@ -2,11 +2,12 @@
 # Processar itens de uma lista até encontrar um valor específico que indica a parada.
 
 parada = 100
-
+i = 0
 lista = [1, 10, 200, 500, 100, 400, 85]
 
-for item in lista:
-    if item != parada:
-        print(item)
-    else:
+while i < len(lista):
+    if lista[i] == parada:
+        print("Encerrando o processamento.")
         break
+    print(f'Processando item nro {lista[i]}')
+    i += 1
